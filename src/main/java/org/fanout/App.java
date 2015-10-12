a036280c3e4c4da16a2cd9535f2822c570161f9a
Konstantin Bokarius <bokarius@comcast.net>
2015-10-12 13:43:43 -0700

    preliminary checkin

---
+++ src/main/java/org/fanout/App.java
@@ -0,0 +1,13 @@
+package org.fanout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
